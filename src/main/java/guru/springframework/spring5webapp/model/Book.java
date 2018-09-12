6c1a445c91ff00b2f2b164e2fe88d5f9b5f9b4ce
Omerf <ceng.of@gmail.com>
2018-09-12 14:18:41 +0300

    Revert "Adding Repository"
    This reverts commit 8f37762

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -13,10 +13,7 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-
-    @OneToOne
-    private Publisher publisher;
-
+    private String publisher;
     @ManyToMany
     @JoinTable(name="author_book",joinColumns=@JoinColumn(name = "book_id"),inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> authors = new HashSet<>();
@@ -24,13 +21,13 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn, Publisher publisher) {
+    public Book(String title, String isbn, String publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -61,11 +58,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public Publisher getPublisher() {
+    public String getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(Publisher publisher) {
+    public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
 
